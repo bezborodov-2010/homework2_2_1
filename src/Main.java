40deeea3048908271e365da95aee9ade2ee28f7f
bezborodov-2010 <bezborodov_2010@mail.ru>
2023-12-02 16:18:07 +0700

    change task 7

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Задача 7");
-
+        var a=5;
+        var b=7;
+        a=a+b-a;
+        System.out.println("a = "+a);
+        b=b-(a/a)-(a/a);
+        System.out.println("b = "+b);
     }
 
 }
