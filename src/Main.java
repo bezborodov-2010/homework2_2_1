6941ca65844146349905af5e702ca2e7e39a28e8
bezborodov-2010 <bezborodov_2010@mail.ru>
2023-12-04 10:39:25 +0700

    change task 7

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
         System.out.println("Задача 7");
         var a=5;
         var b=7;
-        a=a+b-a;
+        a=a+b;
+        b=a-b;
+        a=a-b;
         System.out.println("a = "+a);
-        b=b-(a/a)-(a/a);
         System.out.println("b = "+b);
+
     }
 
 }
